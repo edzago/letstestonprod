1ac7babb789e2f5ac40ea67a7066ec6fb483f114
Edgars Zagorskis <edgars.zagorskis@gmail.com>
2021-04-09 18:59:33 +0300

    #1: Uzlikta pabaude uz vairakiem @ zimemem. Un vispar lietojam regex parbaudi no stackoverflow. peace. imout!

--- public/is-email-valid.js
+++ public/is-email-valid.js
@@ -1,5 +1,10 @@
 //#WEB-345 Es kā lietotājs gribu norādiīt valīdu epasta adresi pie reģistrācijas.
 
-export const isEmailValid = (email) => email.indexOf('@') > 0;
+function stackOverflowRezultataFunkcija(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+export const isEmailValid = (email) => stackOverflowRezultataFunkcija(email);
 
 
--- test/is-email-valid.test.js
+++ test/is-email-valid.test.js
@@ -1,6 +1,10 @@
 import {strict as assert} from 'assert';
 import {isEmailValid} from "../public/is-email-valid.js";
 
+assert(isEmailValid('edgar@sedgars.lv') === true, 'edgar@sedgars.lv nav valīdz epasts');
 assert(isEmailValid('e') === false, 'e nav valīdz epasts');
 assert(isEmailValid('edgarsedgars.lv') === false, 'edgarsedgars.lv nav valīdz epasts');
-assert(isEmailValid('edgar@sedgars.lv') === true, 'edgar@sedgars.lv nav valīdz epasts');
+assert(isEmailValid('edgar@@sedgars.lv') === false, 'edgar@@sedgars.lv nav valīdz epasts');
+assert(isEmailValid('edgar@@sedgars@.lv') === false, 'edgar@@sedgars.lv nav valīdz epasts');
+assert(isEmailValid('edgar@@sedgars@.lv@') === false, 'edgar@@sedgars.lv nav valīdz epasts');
+assert(isEmailValid('@edgar@@sedgars@.lv@') === false, 'edgar@@sedgars.lv nav valīdz epasts');
